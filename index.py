3d83fb1c5fda5f8f03f75b9e0aeab6fbd93fde29
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-02-14 19:00:44 -0500

    messing with bs docs re output

--- index.py
+++ index.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup as bs
 r = requests.get("https://github.com/nezlobnaya")
 
 soup = bs(r.content)
+word_find = soup.find("h2")
 
-
-print(soup.prettify())
+# print(soup.prettify())
+print(word_find)
+print(soup.find_all('title', limit=10))
+print(soup.find(string="nezlobnaya"))
